5989ad771bd4ed872aa655f0488938213918f4d3
Abbas Gholami <abbas.gholami71@gmail.com>
2026-03-24 16:24:26 +0100

    test: update schema test to check for IEntranceInstrument fields

--- tests/schema_packages/test_schema_package.py
+++ tests/schema_packages/test_schema_package.py
@@ -1,11 +1,12 @@
-import os.path
-
-from nomad.client import normalize_all, parse
-
+import os
+from nomad.client.processing import parse
+from nomad.processing.data import normalize_all
 
 def test_schema_package():
     test_file = os.path.join('tests', 'data', 'test.archive.yaml')
     entry_archive = parse(test_file)[0]
     normalize_all(entry_archive)
 
-    assert entry_archive.data.message == 'Hello Markus!'
+    # Assert that our custom schema parsed the ID correctly
+    assert entry_archive.data.fablims_id == 999
+    assert entry_archive.data.manufacturer == 'Test Corp'
