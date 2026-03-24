e1508f67c552faee324d38a560373f9bf2ddce78
Abbas Gholami <abbas.gholami71@gmail.com>
2026-03-24 16:28:31 +0100

    ruff fix

--- tests/schema_packages/test_schema_package.py
+++ tests/schema_packages/test_schema_package.py
@@ -1,6 +1,8 @@
 import os
+
 from nomad.client.processing import parse
 from nomad.processing.data import normalize_all
+
 
 def test_schema_package():
     test_file = os.path.join('tests', 'data', 'test.archive.yaml')
@@ -8,5 +10,5 @@ def test_schema_package():
     normalize_all(entry_archive)
 
     # Assert that our custom schema parsed the ID correctly
-    assert entry_archive.data.fablims_id == 999
+    assert entry_archive.data.fablims_id == 999 # noqa: PLR2004
     assert entry_archive.data.manufacturer == 'Test Corp'
