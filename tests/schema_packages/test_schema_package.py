b5463def111bdcdcc5fd377b6b44bbdac75ca86f
Abbas Gholami <abbas.gholami71@gmail.com>
2026-03-24 16:33:03 +0100

    resolving the error for the pytest

--- tests/schema_packages/test_schema_package.py
+++ tests/schema_packages/test_schema_package.py
@@ -1,14 +1,13 @@
 import os
 
 from nomad.client.processing import parse
-from nomad.processing.data import normalize_all
 
 
 def test_schema_package():
     test_file = os.path.join('tests', 'data', 'test.archive.yaml')
+    # The parse function automatically builds the archive data
     entry_archive = parse(test_file)[0]
-    normalize_all(entry_archive)
 
     # Assert that our custom schema parsed the ID correctly
-    assert entry_archive.data.fablims_id == 999 # noqa: PLR2004
+    assert entry_archive.data.fablims_id == 999  # noqa PLR2004
     assert entry_archive.data.manufacturer == 'Test Corp'
