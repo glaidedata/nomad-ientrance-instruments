2795e092c25f450440953beaf72fd81dda6e81b5
Abbas Gholami <abbas.gholami71@gmail.com>
2026-03-24 16:07:17 +0100

    feat: complete IEntranceInstrument schema with all FabLIMS fields

--- src/ientrance_instruments/schema_packages/__init__.py
+++ src/ientrance_instruments/schema_packages/__init__.py
@@ -2,7 +2,7 @@ from nomad.config.models.plugins import SchemaPackageEntryPoint
 from pydantic import Field
 
 
-class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+class IEntranceSchemaPackageEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
@@ -11,7 +11,7 @@ class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
         return m_package
 
 
-schema_package_entry_point = NewSchemaPackageEntryPoint(
-    name='NewSchemaPackage',
-    description='New schema package entry point configuration.',
+schema_package_entry_point = IEntranceSchemaPackageEntryPoint(
+    name='IEntranceInstrumentsSchema',
+    description='Schema package for iEntrance FabLIMS instruments.',
 )
--- src/ientrance_instruments/schema_packages/schema_package.py
+++ src/ientrance_instruments/schema_packages/schema_package.py
@@ -1,19 +1,12 @@
-from typing import (
-    TYPE_CHECKING,
-)
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from nomad.datamodel.datamodel import (
-        EntryArchive,
-    )
-    from structlog.stdlib import (
-        BoundLogger,
-    )
+    from nomad.datamodel.datamodel import EntryArchive
+    from structlog.stdlib import BoundLogger
 
 from nomad.config import config
-from nomad.datamodel.data import Schema
-from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
-from nomad.metainfo import Quantity, SchemaPackage
+from nomad.datamodel.metainfo.eln import ELNInstrument
+from nomad.metainfo import Quantity, SubSection, Section, SchemaPackage
 
 configuration = config.get_plugin_entry_point(
     'ientrance_instruments.schema_packages:schema_package_entry_point'
@@ -22,17 +15,76 @@ configuration = config.get_plugin_entry_point(
 m_package = SchemaPackage()
 
 
-class NewSchemaPackage(Schema):
-    name = Quantity(
-        type=str, a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity)
+class EquipmentTechnique(Section):
+    """Subsection for techniques supported by the instrument."""
+    technique_id = Quantity(type=int)
+    name = Quantity(type=str)
+    generic_equipment_name = Quantity(type=str)
+    main_category = Quantity(type=str)
+    sub_category = Quantity(type=str)
+
+
+class EquipmentManager(Section):
+    """Subsection for equipment manager contact info."""
+    manager_id = Quantity(type=int)
+    firstname = Quantity(type=str)
+    lastname = Quantity(type=str)
+    email = Quantity(type=str)
+
+
+class IEntranceInstrument(ELNInstrument):
+    """
+    Custom instrument schema for iEntrance.
+    Inherits name, description, datetime, tags, and the instrument_identifiers
+    subsection from ELNInstrument.
+    """
+    m_def = Section(
+        label='iEntrance Instrument',
+        a_eln=dict(lane_width='600px')
     )
-    message = Quantity(type=str)
 
-    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        super().normalize(archive, logger)
+    # --- Core iEntrance/FabLIMS Fields ---
+    fablims_id = Quantity(
+        type=int,
+        description='Persistent ID from FabLIMS.',
+        a_eln=dict(component='NumberEditQuantity')
+    )
+    tenant_id = Quantity(type=int, a_eln=dict(component='NumberEditQuantity'))
+    tenant_name = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    description_html = Quantity(type=str, a_eln=dict(component='RichTextEditQuantity'))
+    manufacturer = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    product_model = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    product_serial_number = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    laboratory_inventory_code = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
 
-        logger.info('NewSchema.normalize', parameter=configuration.parameter)
-        self.message = f'Hello {self.name}!'
+    # --- Status & Access ---
+    equipment_status_id = Quantity(type=int, a_eln=dict(component='NumberEditQuantity'))
+    equipment_status = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    public_view_is_enabled = Quantity(type=bool, a_eln=dict(component='BoolEditQuantity'))
+    equipment_access_policy_name = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    equipment_funding_name = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    equipment_main_contact_info = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+
+    # --- Image Reference ---
+    overview_image = Quantity(
+        type=str,
+        description='Path to the extracted JPG image of the instrument.',
+        a_eln=dict(component='FileEditQuantity')
+    )
+
+    # --- Extended Location Data ---
+    equipment_laboratory_id = Quantity(type=int, a_eln=dict(component='NumberEditQuantity'))
+    equipment_laboratory_name = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    equipment_laboratory_has_building_room_id = Quantity(type=int, a_eln=dict(component='NumberEditQuantity'))
+    equipment_laboratory_has_building_room_name = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    building_room_name = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    building_room_controlled_entrance_id = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    building_floor_name = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+    building_name = Quantity(type=str, a_eln=dict(component='StringEditQuantity'))
+
+    # Nested subsections
+    managers = SubSection(section_def=EquipmentManager, repeats=True)
+    techniques = SubSection(section_def=EquipmentTechnique, repeats=True)
 
 
 m_package.__init_metainfo__()
